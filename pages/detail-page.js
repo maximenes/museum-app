c80eef71005f054ad5f3848d6bc03624abf00241
MaximenesE <maximenes@gmailL.com>
2019-02-13 16:51:31 +0100

    display comments on page

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,24 +1,24 @@
 function submitComment() {
     const inputField = document.getElementById('name')
     const name = inputField.value
-    console.log(name)
-
     const textArea = document.getElementById ('msg')
     const msg = textArea.value
-    console.log(msg)
 
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
-    console.log(h3)
 
     h3.innerHTML = `${name} said:`
     p.innerHTML = msg
 
     comment.classList.add('comment')
-    comment.appenChild.add('h3')
-    comment.appenChild.add('p')
-    
-    console.log(comment)
+    comment.appendChild.add(h3)
+    comment.appendChild.add(p)
+
+    const commentSection = document.getElementById('comments')
+    commentSection.appendChild(comment)
+
+    inputField.value = null
+    textArea.value = null
 
   }
