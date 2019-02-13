f1785f5e5753b8155d9b227922dc03a3139ff550
MaximenesE <maximenes@gmailL.com>
2019-02-13 16:18:57 +0100

    javascript for commentbox

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,4 +1,24 @@
 function submitComment() {
+    const inputField = document.getElementById('name')
+    const name = inputField.value
+    console.log(name)
+
+    const textArea = document.getElementById ('msg')
+    const msg = textArea.value
+    console.log(msg)
+
+    const comment = document.createElement('section')
+    const h3 = document.createElement('h3')
+    const p = document.createElement('p')
+    console.log(h3)
+
+    h3.innerHTML = `${name} said:`
+    p.innerHTML = msg
+
+    comment.classList.add('comment')
+    comment.appenChild.add('h3')
+    comment.appenChild.add('p')
     
-    console.log('Hello!')
+    console.log(comment)
+
   }
